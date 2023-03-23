3e5cc7c580e1518bd361f371ac68fbcc3d5a2282
Lucas Dias <lucaalencarID@gmail.com>
2023-03-22 22:04:46 -0300

    feita a rota `/matches?inProgress=` trazendo todas as partidas filtradas por progresso

--- app/backend/src/controller/matches.controller.ts
+++ app/backend/src/controller/matches.controller.ts
@@ -5,12 +5,20 @@ export default class MatchesController {
   constructor(private service: MatchesService = new MatchesService()) {
   }
 
-  public getAll = async (_req: Request, res: Response) => {
+  public getAll = async (req: Request, res: Response) => {
     try {
+      const { inProgress } = req.query;
+      if (inProgress) {
+        if (inProgress !== 'true' && inProgress !== 'false') {
+          return res.status(400).json({ message: 'inProgress must be "true" or "false"' });
+        }
+        const matchesByProg = await this.service.getMatchesByProg(inProgress);
+        return res.status(200).json(matchesByProg);
+      }
       const matches = await this.service.getMatches();
       return res.status(200).json(matches);
     } catch (e) {
-      return res.status(200).json({ message: 'Internal server error' });
+      return res.status(500).json({ message: 'Internal server error' });
     }
   };
 }
--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -5,17 +5,25 @@ export default class MatchesService {
   constructor(private _matchesModel = MatchesModel) {
   }
 
-  public getMatches = async () => this._matchesModel.findAll({
+  public getMatches = async (): Promise<MatchesModel[]> => this._matchesModel.findAll({
     include:
       [{ model: Teams, as: 'homeTeam', attributes: ['teamName'] },
         { model: Teams, as: 'awayTeam', attributes: ['teamName'] }],
   })
     .then((data: MatchesModel[]) => data.map((elem: MatchesModel) => elem.dataValues));
+
+  public getMatchesByProg = (progress: string): Promise<MatchesModel[]> => this._matchesModel
+    .findAll({
+      include:
+        [{ model: Teams, as: 'homeTeam', attributes: ['teamName'] },
+          { model: Teams, as: 'awayTeam', attributes: ['teamName'] }],
+      where: { inProgress: JSON.parse(progress) },
+    }).then((data: MatchesModel[]) => data.map((elem: MatchesModel) => elem.dataValues));
 }
 
 // const teste = async () => {
 //   const team = new MatchesService();
-//   console.log(await team.getMatches());
+//   console.log(await team.getMatchesByProg('true'));
 // };
 //
 // teste();
